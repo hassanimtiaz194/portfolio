91cf37a6d1930fc7b7c1318fda8d2f40ce2c705c
Hassan-Imtiaz3 <82123163+hassanimtiaz194@users.noreply.github.com>
2024-07-03 20:51:39 +0500

    added basepath in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: isProd ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
