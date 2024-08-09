e05cbbabab3b6b17782629e8876e0b30472ba3a8
Hassan-Imtiaz3 <82123163+hassanimtiaz194@users.noreply.github.com>
2024-08-10 00:45:38 +0500

    fix

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -36,7 +36,7 @@ export default function About() {
           <Image
             // className={styles.logo}
             width={250}
-            height={100}
+            height={250}
             src={`${basePath}/images/about.png`}
             alt="Hassan Imtiaz"
           />
